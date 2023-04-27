4ffc7d90912e5c41a405a5342e8abc91652075d0
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-04-27 08:27:13 +0300

    done asignmnet

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes,} from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
+import { createMemoryHistory } from "history";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+     <NavBar/>
+     <Routes>
+    
+       <Route path="/" element = {<Home/>}/>
+       <Route path="actors" element = {<Actors/>}/>
+       <Route path="directors" element = {<Directors/>}/>
+       <Route path="movies" element = {<Movies/>}/>
+     </Routes>
+  </div>
+  )
 }
 
 export default App;
